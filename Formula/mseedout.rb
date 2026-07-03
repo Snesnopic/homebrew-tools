bd9c91b1701e038be9b8d5efb38c3c52690de073
Snesnopic <snesnopic@gmail.com>
2026-07-03 13:59:20 +0200

    Update mseedout

--- Formula/mseedout.rb
+++ Formula/mseedout.rb
@@ -1,9 +1,11 @@
 class Mseedout < Formula
   desc "Recompress miniSEED files to the smallest possible valid output using dynamic programming"
   homepage "https://github.com/Snesnopic/mseedout"
-  url "https://github.com/Snesnopic/mseedout/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "abaebeb75a2c3192178725464d9cdba417748c9c5bdf123aa34bb71518ae4a43"
-  license "MIT"
+    url "https://github.com/Snesnopic/mseedout.git",
+        tag:      "v1.0.0",
+        using:    :git,
+        submodules: true
+    license "MIT"
 
   depends_on "cmake" => :build
 
