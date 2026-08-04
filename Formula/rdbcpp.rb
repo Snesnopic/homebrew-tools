2db42925bbf037c7f8cea99575a806e71a45a890
Snesnopic <snesnopic@gmail.com>
2026-08-04 11:51:58 +0200

    Update rdbcpp

--- Formula/rdbcpp.rb
+++ Formula/rdbcpp.rb
@@ -1,8 +1,8 @@
 class Rdbcpp < Formula
   desc "Tencent RDB archive packer/unpacker"
   homepage "https://github.com/Snesnopic/rdbcpp"
-  url "https://github.com/Snesnopic/rdbcpp/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e8e491cf2bbe4825d03a728d01b649f6ab7d05f90f712b4ff13ed312d925b773"
+  url "https://github.com/Snesnopic/rdbcpp/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "23810a3e8f5f78fd815b439e325da265a2ca2dd4db8b809e9ec26371cc8113a2"
   license "MIT"
 
   depends_on "cmake" => :build
