9142b00341e9a79e9b437dc7321817e56ffb03fc
Snesnopic <snesnopic@gmail.com>
2026-08-16 00:24:54 +0200

    Update optigif

--- Formula/optigif.rb
+++ Formula/optigif.rb
@@ -1,9 +1,8 @@
 class Optigif < Formula
   desc "Losslessly optimize GIF files"
   homepage "https://github.com/Snesnopic/optigif"
-  url "https://github.com/Snesnopic/optigif.git",
-      tag:   "v1.0.0",
-      using: :git
+  url "https://github.com/Snesnopic/optigif/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "cb9ded3773a2298786ef5abe286fd16343b0e384f406e84b4d743ff3301a941a"
   license "MIT"
 
   depends_on "cmake" => :build
